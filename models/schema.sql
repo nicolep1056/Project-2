12c92d3d24ed412e9cf902354b6d448fc9f4558e
nicolep1056 <nicolep1056@gmail.com>
2018-11-17 14:09:36 -0600

    basic schema, db name, etc

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS grandmas_attic;
+CREATE DATABASE grandmas_attic;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE grandmas_attic
+
+CREATE TABLE junk(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    TIMESTAMP,
+    days_available INT NULL,
+    PRIMARY KEY(id)
+);
+
+USE grandmas_attic;
+CREATE TABLE items(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    item VARCHAR(100),
+    PRIMARY KEY(id)
+);
