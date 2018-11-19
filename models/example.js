0b2b211a3f936be962058d8027e51ec9458587a1
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-19 07:46:38 -0600

    work on routing

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Item = sequelize.define("Item", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Item;
 };
