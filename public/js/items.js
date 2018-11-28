e62c37778c6fda50060612a6e15c76b0da39f0f6
artooth <artooth@gmail.com>
2018-11-27 19:03:55 -0600

    holiday card buttons working

--- public/js/items.js
+++ public/js/items.js
@@ -1,13 +1,14 @@
-document.getElementById("christmas").addEventListener("click", function () {
-    app.get("/api/christmas/:christmas"){
-        Item.findAll({
-            where: {
-                season: req.params.season
-            }
-        }).then(function (result) {
-            return res.json(result);
-        });
-    }
+console.log("Loaded")
+$(".cat").on("click", function () {
 
-    var = `<h1>${}<h1>`
-});
+    let clickVal = $(this).html().split(" ").join("").toLowerCase();
+    console.log('Value clicked on is: ', clickVal);
+
+    $.get("/api/holidays/" + clickVal, function (items) {
+        // items is your data from the back end to use to build your html like...
+        //$(.).append html here
+    })
+})
+
+// var = `<h1>${}<h1>`
+// });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,39 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all items
-  app.get("/api/items", function(req, res) {
-    db.Item.findAll({}).then(function(dbItems) {
+  app.get("/api/items", function (req, res) {
+    db.Item.findAll({}).then(function (dbItems) {
       res.json(dbItems);
     });
   });
 
   // Create a new item
-  app.post("/api/items", function(req, res) {
-    db.Item.create(req.body).then(function(dbItem) {
+  app.post("/api/items", function (req, res) {
+    db.Item.create(req.body).then(function (dbItem) {
       res.json(dbItem);
     });
   });
 
   // Delete an item by id
-  app.delete("/api/items/:id", function(req, res) {
-    db.Item.destroy({ where: { id: req.params.id } }).then(function(dbItem) {
+  app.delete("/api/items/:id", function (req, res) {
+    db.Item.destroy({ where: { id: req.params.id } }).then(function (dbItem) {
       res.json(dbItem);
     });
   });
+
+  /*
+  app.get("/api/holidays/:cat", function(req,res){
+    let searchTerm = req.params.cat
+
+    Item.findAll({
+            where: {
+                season:searchTerm
+            }
+            res.send(data)
+
+  })
+  
+  
+  */
 };
