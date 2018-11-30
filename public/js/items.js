95d19d7afeaec0b57edb4319efd729e661219e37
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-30 00:48:32 -0600

    update html

--- public/js/items.js
+++ public/js/items.js
@@ -1,3 +1,5 @@
+
+
 //Determine which occasion is clicked on and direct to the appropriate page.
 $('.card-img-top').on('click', function () {
     let seasonSelected = $(this).attr("id");
@@ -15,20 +17,18 @@ $(document).on('click','.claimBtn',function(){
     let itemClaimed = $(this).attr("id");
         console.log('Item Claimed (id): ', itemClaimed) 
         //if time allows, create modal to ask if user is sure they want to claim a specific item
-
+        
             // Send the DELETE request.
-            $.ajax("/api/id/" + itemClaimed, {
+            /*$.ajax("/api/id/" + itemClaimed, {
               type: "DELETE"
             }).then(
               function() {
                 console.log("claimed item", id);
                 // Reload the page to get the updated list
-                location.reload();
+                //location.reload();
               }
-            );
+            );*/
           });
-       /* Item.destroy({
-            where: {id: itemClaimed}
-          })
-          location.reload()*/
+
+
 
